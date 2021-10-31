c368279e91c52d4c16bee95e85e3680214963edc
Reynolds <l_e_reynolds@yahoo.co.uk>
2021-10-31 11:00:07 +0000

    Small refactor of Parser and Scraper class

--- tower_bridge_lift_scraper.py
+++ tower_bridge_lift_scraper.py
@@ -5,10 +5,12 @@ Created on Mon Oct 25 21:04:38 2021
 """
 import datetime
 import requests
+import logging
 
 from bs4 import BeautifulSoup
 
 class Scraper():
+    """Scraper class to get response object from HTTP request."""
     
     TARGET_URL = "https://www.towerbridge.org.uk/lift-times"
     TIMEOUT = 5
@@ -16,31 +18,47 @@ class Scraper():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
         }
     
-    def __init__(self):
-        pass
+    def __init__(self, target_url):
+        self.target_url = target_url
     
     def scrape(self):
+        """
+        Return response object from HTTTP request if successful.
+               
+        If any error occurs, we log appropriately and then exit.        
+        
+        Raises
+        ------
+        SystemExit
+            DESCRIPTION.
+
+        Returns
+        -------
+        requests.Response
+            Response of the HTTP request made if successful, None otherwise.
+        """
         try:
-            response = requests.get(Scraper.TARGET_URL,\
+            response = requests.get(self.target_url,\
                                     headers=Scraper.HEADERS, \
                                     timeout=Scraper.TIMEOUT)
             response.raise_for_status()
         except requests.exceptions.HTTPError as e:
-            print(e)
-            return None
+            # TODO: Log appropriately
+            raise SystemExit(e)
         except requests.exceptions.ConnectionError as e:
+            # TODO: Log appropriately
             print("Connection Failed.")
             print(e)
-            return None
+            raise SystemExit(e)
         except requests.exceptions.RequestException as e:
-            # Ambigous error, bail
+            # TODO: Log appropriately
             raise SystemExit(e)
         
         return response.text
 
 
 class Parser():
-    
+    """Parser class to pass HTML from Scraper."""
     
     def __init__(self):
         """Construct an empty Parser object."""
@@ -63,7 +81,7 @@ class Parser():
             date = event[1].text.strip()
             time = event[2].text.strip()
             vessel = event[3].text.strip()
-            if self.to_datetime(date) < cur_date:  # only show for this week
+            if self._to_date(date) < cur_date:  # only show for this week
                 events.append(LiftEvent(date, time, vessel))
     
         return events
@@ -72,7 +90,7 @@ class Parser():
         return soup.find("table", class_="views-table")
     
     @staticmethod
-    def to_datetime(date_string):
+    def _to_date(date_string):
         """Aux function to convert date as string into datetime object."""
         date_object = datetime.datetime.strptime(date_string, "%d %b %Y").date()
         return date_object
